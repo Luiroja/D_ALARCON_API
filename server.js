4f4f515d06eb7777128eab671322dfc225bd53cd
Rogerio Juarez <luis22uteq@gmail.com>
2023-02-03 14:59:17 -0500

    Routes, Models and auth-route had been creates

--- server.js
+++ server.js
@@ -4,11 +4,16 @@ const connectDB = require('./config/db')
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
 
+const userRoute = require('./routes/user')
+const authRoute = require('./routes/auth')
+
 connectDB()
 const app = express();
 
 app.use (express.json());
 app.use (express.urlencoded({ extended: false }));
+app.use ("/api/users", userRoute)
+app.use ("/api/auth", authRoute)
 
 
 app.listen(port, ()=>console.log(`El servidor se inició en el puerto ${port}`))
