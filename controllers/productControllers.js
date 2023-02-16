e558b6e012c183beb268520bd262520db8311959
Rogerio Juarez <luis22uteq@gmail.com>
2023-02-16 18:58:42 -0500

    Solving Product model

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -45,7 +45,7 @@ const getProduct = asyncHandler(async (req, res) => {
         res.status(400)
         throw new Error ("Producto no encontrado")
     }else {
-        const getProduct =await User.findById (
+        const getProduct =await Product.findById (
             req.params.id,
             req.body,)
             res.status(200).json(getProduct)
